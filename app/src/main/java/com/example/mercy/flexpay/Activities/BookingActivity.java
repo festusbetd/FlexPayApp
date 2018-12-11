5e15d65fe9af9ee65f519b703439fe4c0eae55b7
festusbetd <festusbet@gmail.com>
2018-12-11 11:43:09 +0300

    added line grahp

--- app/src/main/java/com/example/mercy/flexpay/Activities/BookingActivity.java
+++ app/src/main/java/com/example/mercy/flexpay/Activities/BookingActivity.java
@@ -1,6 +1,7 @@
 package com.example.mercy.flexpay.Activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,8 +16,21 @@ import com.example.mercy.flexpay.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+
 public class BookingActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private Spinner spinner_status,spinner_show;
+    LineChartView lineChartView;
+    String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
+            "Oct", "Nov", "Dec"};
+    int[] yAxisData = {0, 1, 3, 3, 4, 3, 4, 5, 6,6 , 7, 7};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +50,52 @@ public class BookingActivity extends AppCompatActivity implements AdapterView.On
                 finish();
             }
         });
+
+
+        lineChartView = findViewById(R.id.chart);
+
+
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#005B77"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#005B77"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Number of Booking");
+        yAxis.setTextColor(Color.parseColor("#066e8a"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 10;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+
+
+
 
         // Spinner Drop down elements
         List<String> categories = new ArrayList<String>();
